01b9ec4b40cdebbd7e9dce4de481d6458511d6b7
Kaluzhskiy <knightofnight@mail.ru>
2019-07-11 18:38:26 +0300

    Залил макет с урока 3

---
+++ Калужский Александр/js/main.js
@@ -0,0 +1,68 @@
+const API = 'https://raw.githubusercontent.com/GeekBrainsTutorial/online-store-api/master/responses';
+
+class ProductsList {
+    constructor(container = '.products'){
+        this.container = container;
+        this.goods = [];
+        this.allProducts = [];
+        this._getProducts()
+            .then(data => {
+                this.goods = [...data];
+                this.render();
+            });
+    }
+
+    _getProducts() {
+        return fetch(`${API}/catalogData.json`)
+            .then(result => result.json())
+            .catch(error => {
+                console.log(error);
+            });
+    }
+
+    /**
+     * Функция считает суммарную стоимость всех товаров
+     * @returns {number}
+     */
+    sumAllProducts() {
+        let sumAll = 0;
+        for(let i=0; i<this.goods.length; i++) {
+            sumAll = sumAll + this.goods[i].price;
+        }
+        return sumAll;
+    }
+    calcSum() {
+        return this.allProducts.reduce((accum, item) => accum += item.price, 0);
+    }
+
+    render(){
+        const block = document.querySelector(this.container);
+        for (let product of this.goods){
+            const productObj = new ProductItem(product);
+            this.allProducts.push(productObj);
+            block.insertAdjacentHTML('beforeend', productObj.render());
+        }
+    }
+}
+
+class ProductItem {
+    constructor(product, img = 'https://placehold.it/200x150'){
+        this.title = product.product_name;
+        this.price = product.price;
+        this.id = product.id;
+        this.img = img;
+    }
+    render(){
+        return `<div class="product-item" data-id="${this.id}">
+                <img src="${this.img}" alt="Some img">
+                <div class="desc">
+                    <h3>${this.title}</h3>
+                    <p>${this.price} \u20bd</p>
+                    <button class="buy-btn" onclick="alert('товар добавлен в корзину!')">Купить</button>
+                </div>
+            </div>`
+    }
+}
+
+let list = new ProductsList();
+list.render();
