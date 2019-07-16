43c47ffc579d72fdade9e462c1123782042a0d4a
Kaluzhskiy <knightofnight@mail.ru>
2019-07-16 15:40:49 +0300

    Подготовка к ветвлению

--- Калужский Александр/js/main.js
+++ Калужский Александр/js/main.js
@@ -1,5 +1,26 @@
 const API = 'https://raw.githubusercontent.com/GeekBrainsTutorial/online-store-api/master/responses';
 
+/*let getRequest = (url) => {
+    return new Promise((resolve, reject) => {
+        let xhr = new XMLHttpRequest();
+        xhr.open("GET", url, true);
+        xhr.onreadystatechange = () => {
+            if(xhr.readyState === 4){
+                if(xhr.status !== 200){
+                    reject('Error');
+                } else {
+                    resolve(xhr.responseText);
+                 }
+            }
+        };
+        xhr.send();
+    })
+};
+
+getRequest('url').then(data => {
+
+}).catch();*/
+
 class ProductsList {
     constructor(container = '.products'){
         this.container = container;
