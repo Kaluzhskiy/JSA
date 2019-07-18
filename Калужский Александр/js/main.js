2060c816d92101bc2cd802a89df8c9650d7eda85
Kaluzhskiy <knightofnight@mail.ru>
2019-07-18 12:53:27 +0300

    Homework at lessons 3

--- Калужский Александр/js/main.js
+++ Калужский Александр/js/main.js
@@ -1,94 +1,4 @@
-const API = 'https://raw.githubusercontent.com/GeekBrainsTutorial/online-store-api/master/responses';
-
-/*let getRequest = (url) => {
-    return new Promise((resolve, reject) => {
-        let xhr = new XMLHttpRequest();
-        xhr.open("GET", url, true);
-        xhr.onreadystatechange = () => {
-            if(xhr.readyState === 4){
-                if(xhr.status !== 200){
-                    reject('Error');
-                } else {
-                    resolve(xhr.responseText);
-                 }
-            }
-        };
-        xhr.send();
-    })
-};
-
-getRequest('url').then(data => {
-
-}).catch();*/
-
-/*
-class ProductsList {
-    constructor(container = '.products'){
-        this.container = container;
-        this.goods = [];
-        this.allProducts = [];
-        this._getProducts()
-            .then(data => {
-                this.goods = [...data];
-                this.render();
-            });
-    }
-
-    _getProducts() {
-        return fetch(`${API}/catalogData.json`)
-            .then(result => result.json())
-            .catch(error => {
-                console.log(error);
-            });
-    }
-
-    /!**
-     * Функция считает суммарную стоимость всех товаров
-     * @returns {number}
-     *!/
-    sumAllProducts() {
-        let sumAll = 0;
-        for(let i=0; i<this.goods.length; i++) {
-            sumAll = sumAll + this.goods[i].price;
-        }
-        return sumAll;
-    }
-    calcSum() {
-        return this.allProducts.reduce((accum, item) => accum += item.price, 0);
-    }
-
-    render(){
-        const block = document.querySelector(this.container);
-        for (let product of this.goods){
-            const productObj = new ProductItem(product);
-            this.allProducts.push(productObj);
-            block.insertAdjacentHTML('beforeend', productObj.render());
-        }
-    }
-}
-
-class ProductItem {
-    constructor(product, img = 'https://placehold.it/200x150'){
-        this.title = product.product_name;
-        this.price = product.price;
-        this.id = product.id;
-        this.img = img;
-    }
-    render(){
-        return `<div class="product-item" data-id="${this.id}">
-                <img src="${this.img}" alt="Some img">
-                <div class="desc">
-                    <h3>${this.title}</h3>
-                    <p>${this.price} \u20bd</p>
-                    <button class="buy-btn" onclick="alert('товар добавлен в корзину!')">Купить</button>
-                </div>
-            </div>`
-    }
-}
-
-let list = new ProductsList();
-list.render();*/
-
+const API = 'https://raw.githubusercontent.com/Kaluzhskiy/online-store-api/master/responses';
 
 class List {
     constructor(url, container, list = list2){
@@ -142,22 +52,23 @@ class List {
 }
 
 class Item{
-    constructor(el, img = 'https://placehold.it/200x150'){
+    constructor(el){
         this.product_name = el.product_name;
         this.price = el.price;
         this.id_product = el.id_product;
-        this.img = img;
+        this.img = el.img;
     }
     render(){
         return `<div class="product-item" data-id="${this.id_product}">
                 <img src="${this.img}" alt="Some img">
                 <div class="desc">
                     <h3>${this.product_name}</h3>
-                    <p>${this.price} $</p>
+                    <p>${this.price} \u20bd</p>
                     <button class="buy-btn"
                     data-id="${this.id_product}"
                     data-name="${this.product_name}"
-                    data-price="${this.price}">Купить</button>
+                    data-price="${this.price}"
+                    data-src="${this.img}">Купить</button>
                 </div>
             </div>`
     }
@@ -207,6 +118,7 @@ class Cart extends List{
                             id_product: productId,
                             price: +element.dataset['price'],
                             product_name: element.dataset['name'],
+                            img: element.dataset.src,
                             quantity: 1
                         };
                         this.goods = [product];
@@ -237,8 +149,8 @@ class Cart extends List{
     }
     _updateCart(product){
         let block = document.querySelector(`.cart-item[data-id="${product.id_product}"]`);
-        block.querySelector('.product-quantity').textContent = `Quantity: ${product.quantity}`;
-        block.querySelector('.product-price').textContent = `$${product.quantity*product.price}`;
+        block.querySelector('.product-quantity').textContent = `Колличество: ${product.quantity}`;
+        block.querySelector('.product-price').textContent = `\u20bd${product.quantity*product.price}`;
     }
     _init(){
         document.querySelector('.btn-cart').addEventListener('click', () => {
@@ -264,12 +176,12 @@ class CartItem extends Item{
             <img src="${this.img}" alt="Some image">
             <div class="product-desc">
             <p class="product-title">${this.product_name}</p>
-            <p class="product-quantity">Quantity: ${this.quantity}</p>
-        <p class="product-single-price">$${this.price} each</p>
+            <p class="product-quantity">Колличество: ${this.quantity}</p>
+        <p class="product-single-price">\u20bd${this.price} за один</p>
         </div>
         </div>
         <div class="right-block">
-            <p class="product-price">$${this.quantity*this.price}</p>
+            <p class="product-price">\u20bd${this.quantity*this.price}</p>
             <button class="del-btn" data-id="${this.id_product}">&times;</button>
         </div>
         </div>`
